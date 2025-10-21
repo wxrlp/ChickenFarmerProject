ad98d4fd6619c2559ffe05a646e95f779b50fad9
Adin Sreekesh <adin.sreekesh@gmail.com>
2025-10-21 17:31:11 +1000

    Added javadocs and comments

--- src/builder/entities/npc/spawners/BeeHiveSpawner.java
+++ src/builder/entities/npc/spawners/BeeHiveSpawner.java
@@ -3,56 +3,81 @@ package builder.entities.npc.spawners;
 import builder.GameState;
 import builder.entities.npc.BeeHive;
 
+import builder.entities.npc.NpcManager;
+import builder.player.Player;
 import engine.EngineState;
 import engine.timing.RepeatingTimer;
 import engine.timing.TickTimer;
 
+/**
+ * Spawner for BeeHive bees
+ */
 public class BeeHiveSpawner implements Spawner {
 
-    RepeatingTimer timer;
-    private int x = 0;
-    private int y = 0;
+    private final RepeatingTimer timer;
+    private int x;
+    private int y;
 
+    /**
+     * Creates a new BeeHive spawner at the given coordinates
+     *
+     * @param x The x coordinate of the spawner
+     * @param y The y coordinate of the spawner
+     * @param duration The duration between spawns
+     */
     public BeeHiveSpawner(int x, int y, int duration) {
         this.x = x;
         this.y = y;
         this.timer = new RepeatingTimer(duration);
     }
 
+    /** Returns the timer for this spawner */
     @Override
     public TickTimer getTimer() {
         return this.timer;
     }
 
+    /**
+     * Tick method for the spawner
+     *
+     * @param state The current engine state
+     * @param game  The current game state
+     */
     @Override
     public void tick(EngineState state, GameState game) {
-        timer.tick();
+        Player player = game.getPlayer();
+        NpcManager npcs = game.getNpcs();
         final boolean canAfford =
-                game.getInventory().getFood() >= 3 && game.getInventory().getCoins() >= 3;
+                game.getInventory().getFood() >= BeeHive.FOOD_COST
+                        && game.getInventory().getCoins() >= BeeHive.COIN_COST;
 
         if (canAfford && state.getKeys().isDown('h')) {
-            game.getInventory().addFood(-3);
-            game.getInventory().addCoins(-3);
-            game.getNpcs().addNpc(new BeeHive(game.getPlayer().getX(), game.getPlayer().getY()));
+            game.getInventory().addFood(BeeHive.FOOD_COST);
+            game.getInventory().addCoins(BeeHive.COIN_COST);
+            npcs.addNpc(new BeeHive(player.getX(), player.getY()));
         }
         // look at use code example to spawn based on user input and only on grass tiles
     }
 
+    /** returns the x coordinate of the spawner */
     @Override
     public int getX() {
         return this.x;
     }
 
+    /** sets the x coordinate of the spawner */
     @Override
     public void setX(int x) {
         this.x = x;
     }
 
+    /** returns the y coordinate of the spawner */
     @Override
     public int getY() {
         return this.y;
     }
 
+    /** sets the y coordinate of the spawner */
     @Override
     public void setY(int y) {
         this.y = y;
--- src/builder/entities/npc/spawners/PigeonSpawner.java
+++ src/builder/entities/npc/spawners/PigeonSpawner.java
@@ -1,6 +1,7 @@
 package builder.entities.npc.spawners;
 
 import builder.GameState;
+import builder.entities.npc.enemies.EnemyManager;
 import builder.entities.resources.Cabbage;
 import builder.entities.tiles.Tile;
 
@@ -11,21 +12,29 @@ import engine.timing.RepeatingTimer;
 
 import java.util.List;
 
+/**
+ * Spawner that spawns pigeons when there are cabbages on the map.
+ */
 public class PigeonSpawner extends HelperSpawner implements Spawner {
     private static final int DEFAULT_SPAWN_INTERVAL = SpawnerBaseValues.PIGEON_SPAWN_INTERVAL;
+
+    /** Creates a pigeon spawner at the given coordinates with the default spawn interval. */
     public PigeonSpawner(int x, int y) {
         super(x, y, DEFAULT_SPAWN_INTERVAL);
     }
 
+    /** Creates a pigeon spawner at the given coordinates with a custom spawn interval. */
     public PigeonSpawner(int x, int y, int duration) {
         super(x, y, new RepeatingTimer(duration));
     }
 
 
-
+    /**  * Spawns a pigeon targeting the closest cabbage if any cabbages exist on the map. */
     @Override
     public void tick(EngineState state, GameState game) {
+        EnemyManager enemies = game.getEnemies();
         timer.tick();
+        // Select all tiles that have cabbages on them
         List<Tile> tiles =
                 game.getWorld()
                         .tileSelector(
@@ -38,7 +47,8 @@ public class PigeonSpawner extends HelperSpawner implements Spawner {
                                     return false;
                                 });
 
-        if (tiles.size() > 0) {
+        // Find the closest cabbage tile
+        if (!tiles.isEmpty()) {
             int distance = this.distanceFrom(tiles.getFirst());
             Tile closest = tiles.getFirst();
             for (Tile tile : tiles) {
@@ -47,10 +57,11 @@ public class PigeonSpawner extends HelperSpawner implements Spawner {
                 }
             }
 
+            // Spawn a pigeon targeting the closest cabbage if the timer is finished
             if (this.getTimer().isFinished()) {
-                game.getEnemies().spawnX = this.getX();
-                game.getEnemies().spawnY = this.getY();
-                game.getEnemies().Birds.add(game.getEnemies().makePigeon(closest));
+                enemies.spawnX = this.getX();
+                enemies.spawnY = this.getY();
+                enemies.Birds.add(game.getEnemies().makePigeon(closest));
             }
         }
     }
