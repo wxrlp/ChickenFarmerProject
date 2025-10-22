f5ab3cacbe8545b6951bddf6280bb3b59f537e2d
Adin Sreekesh <adin.sreekesh@gmail.com>
2025-10-22 21:10:24 +1000

    Added javadocs

--- src/builder/world/CabbageDetails.java
+++ src/builder/world/CabbageDetails.java
@@ -2,6 +2,11 @@ package builder.world;
 
 import engine.game.ImmutablePosition;
 
+/**
+ * CabbageDetails interface representing the immutable position of
+ * a cabbage in the game world.
+ * Useful for future cabbage-related implementations
+ */
 public interface CabbageDetails extends ImmutablePosition {
 
 }
