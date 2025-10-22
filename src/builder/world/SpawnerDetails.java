0ecec4f92e2d874758af6684d40e820bc4f2f790
Adin Sreekesh <adin.sreekesh@gmail.com>
2025-10-22 21:11:34 +1000

    Improved javadocs

--- src/builder/world/SpawnerDetails.java
+++ src/builder/world/SpawnerDetails.java
@@ -1,15 +1,8 @@
 package builder.world;
 
 import engine.game.HasPosition;
+import engine.game.ImmutablePosition;
 
 public interface SpawnerDetails extends HasPosition {
-    public int getX();
-
-    public int getY();
-
-    public void setX(int x);
-
-    public void setY(int y);
-
-    public int getDuration();
+    int getDuration();
 }
