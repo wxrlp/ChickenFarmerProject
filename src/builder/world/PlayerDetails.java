e04a3613a0d2846066910a7e3742e05a7ef99cdd
Adin Sreekesh <adin.sreekesh@gmail.com>
2025-10-22 21:11:10 +1000

    Added javadocs

--- src/builder/world/PlayerDetails.java
+++ src/builder/world/PlayerDetails.java
@@ -2,6 +2,10 @@ package builder.world;
 
 import engine.game.ImmutablePosition;
 
+/**
+ * PlayerDetails interface representing the immutable position and
+ * starting resources of a player in the game world.
+ */
 public interface PlayerDetails extends ImmutablePosition {
     int getStartingFood();
 
