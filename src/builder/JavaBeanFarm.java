979fda27bea44d80cc4b83d03731a17264ee5978
Adin Sreekesh <adin.sreekesh@gmail.com>
2025-10-22 22:40:17 +1000

    Massively shortened constructor via helper methods

--- src/builder/JavaBeanFarm.java
+++ src/builder/JavaBeanFarm.java
@@ -56,15 +56,24 @@ import java.util.StringJoiner;
 public class JavaBeanFarm implements Game {
 
     private final PlayerManager playerManager;
-
+    private static final int INVENTORY_SIZE = 5;
     private final NpcManager npcs;
     private final EnemyManager enemies;
-
+    private static final int CABBAGE_INVENTORY_SIZE = 5;
+    private static final int CABBAGE_INVENTORY_COINS = 100;
+    private static final int CABBAGE_INVENTORY_FOOD = 100;
     private final BeanWorld world;
 
     private final Inventory inventory;
     private final List<Overlay> overlays = new ArrayList<>();
 
+
+    /** * Reads all content from a reader and returns it as a single
+     * string.
+     * @param reader The reader to read from.
+     * @return The content read from the reader as a string.
+     * @throws IOException If an I/O error occurs.
+     */
     private String readAllReader(Reader reader) throws IOException {
         BufferedReader br = new BufferedReader(reader);
         StringJoiner sb = new StringJoiner(System.lineSeparator());
@@ -74,7 +83,6 @@ public class JavaBeanFarm implements Game {
         }
         return sb.toString();
     }
-
 
 
     /**
@@ -93,10 +101,11 @@ public class JavaBeanFarm implements Game {
      *                            cannot be parsed successfully.
      */
     public JavaBeanFarm(Dimensions dimensions, Reader mapReader,
-                        Reader detailReader)
+                        Reader detailReader, BeanWorld world)
             throws IOException, WorldLoadException {
-
+        this.world = world;
         final String detailsContent = readAllReader(detailReader);
+        final String worldContent = readAllReader(mapReader);
         final PlayerDetails playerDetails =
                 OverlayBuilder.getPlayerDetailsFromFile(
                         detailsContent);
@@ -106,66 +115,119 @@ public class JavaBeanFarm implements Game {
         this.npcs = new NpcManager();
         this.enemies = new EnemyManager(dimensions);
         loadSpawners(
-                OverlayBuilder.getMagpieSpawnDetailsFromString(detailsContent),
+                OverlayBuilder.getMagpieSpawnDetailsFromString(
+                        detailsContent),
                 MagpieSpawner::new);
         loadSpawners(
-                OverlayBuilder.getEagleSpawnDetailsFromString(detailsContent),
+                OverlayBuilder.getEagleSpawnDetailsFromString(
+                        detailsContent),
                 EagleSpawner::new);
         loadSpawners(
-                OverlayBuilder.getPigeonSpawnDetailsFromString(detailsContent),
+                OverlayBuilder.getPigeonSpawnDetailsFromString(
+                        detailsContent),
                 PigeonSpawner::new);
 
-        String worldContent = readAllReader(mapReader);
-        this.world = WorldBuilder.fromTiles(
-                WorldBuilder.fromString(dimensions, worldContent));
+        initialiseCabbage(detailsContent, dimensions);
+        this.inventory = createInventory(playerDetails);
+        inventoryInitialiser(this.inventory);
+        initializeOverlays(dimensions);
 
-        final List<CabbageDetails> cabbageSpawnPoints =
-                OverlayBuilder.getCabbageSpawnDetailsFromString(
-                        detailsContent);
-        for (CabbageDetails cabbageDetails : cabbageSpawnPoints) {
-            // HACK - can I improve this?
-            final int positionX = cabbageDetails.getX();
-            final int positionY = cabbageDetails.getY();
-            final List<Tile> tiles =
-                    this.world.tilesAtPosition(
-                            positionX, positionY,
-                            dimensions);
-            for (Tile tile : tiles) {
-                if (tile instanceof Dirt) {
-                    TinyInventory tempInventory =
-                            new TinyInventory(5, 100, 100);
-                    ((Dirt) tile).till();
-                    ((Dirt) tile).plant(tempInventory);
-                }
-            }
-        }
-
-        int inventorySize = 5;
-        this.inventory = new TinyInventory(
-                inventorySize, playerDetails.getStartingCoins(),
-                playerDetails.getStartingFood());
-
-        inventoryInitialiser(inventory);
-
-        this.overlays.add(
-                new InventoryOverlay(dimensions, inventorySize));
-        this.overlays.add(new ResourceOverlay(dimensions));
     }
 
+
+    /**
+     * Constructs a new JavaBean Farm game using the given
+     * dimensions, mapPath and detailPath
+     * @param dimensions The dimensions we want for this game.
+     * @param mapFile The path to a file that contains a
+     *                description of the world map.
+     * @param detailsFile The path to a file that contains the
+     *                    overlay details for the game, e.g.
+     *                    spawner locations.
+     * @param world The world instance to use in the game.
+     * @throws IOException     If the game is unable to find or
+     *                       open the default world map file.
+     * @throws WorldLoadException If the default world map file
+     *                            cannot be parsed successfully.
+     */
     public JavaBeanFarm(Dimensions dimensions, String mapFile,
-                        String detailsFile)
+                        String detailsFile, BeanWorld world)
             throws IOException, WorldLoadException {
         this(
                 dimensions, new FileReader(mapFile),
-                new FileReader(detailsFile));
+                new FileReader(detailsFile), world);
     }
 
+    /** * Initialises cabbages in the world based on the details
+     * content.
+     * @param detailsContent The details content to parse for
+     *                       cabbage spawn points.
+     * @param dimensions The dimensions of the world.
+     * @throws IOException If an I/O error occurs.
+     */
+    private void initialiseCabbage(String detailsContent,
+                                   Dimensions dimensions)
+            throws IOException {
+        List<CabbageDetails> cabbageSpawnPoints =
+                OverlayBuilder.getCabbageSpawnDetailsFromString(
+                        detailsContent);
+        for (CabbageDetails cabbageDetails : cabbageSpawnPoints) {
+            plantCabbageAt(
+                    cabbageDetails.getX(),
+                    cabbageDetails.getY(),
+                    dimensions);
+        }
+    }
 
+    /** * Plants a cabbage at the given pixel coordinates if
+     * possible.
+     * @param x The x coordinate in pixels.
+     * @param y The y coordinate in pixels.
+     * @param dimensions The dimensions of the world.
+     */
+    private void plantCabbageAt(int x, int y,
+                                Dimensions dimensions) {
+        List<Tile> tiles =
+                this.world.tilesAtPosition(
+                        x, y,
+                        dimensions);
+        for (Tile tile : tiles) {
+            if (tile instanceof Dirt) {
+                TinyInventory tempInventory =
+                        new TinyInventory(
+                                CABBAGE_INVENTORY_SIZE,
+                                CABBAGE_INVENTORY_COINS,
+                                CABBAGE_INVENTORY_FOOD);
+                ((Dirt) tile).till();
+                ((Dirt) tile).plant(tempInventory);
+            }
+        }
+    }
+
+    /** * Creates an inventory for the player based on the
+     * player details.
+     * @param playerDetails The player details to use.
+     * @return The created inventory.
+     */
+    private Inventory createInventory(PlayerDetails playerDetails) {
+        return new TinyInventory(
+                INVENTORY_SIZE,
+                playerDetails.getStartingCoins(),
+                playerDetails.getStartingFood());
+    }
+
+    /**
+     * A factory interface for creating spawners.
+     */
     @FunctionalInterface
     private interface SpawnerFactory {
         Spawner create(int x, int y, int duration);
     }
 
+    /**
+     * Loads spawners from the given details using the provided
+     * factory.
+     */
     private void loadSpawners(
             List<SpawnerDetails> spawnerDetails,
             SpawnerFactory factory) {
@@ -177,6 +239,18 @@ public class JavaBeanFarm implements Game {
         }
     }
 
+    /** * Initializes the overlays for the game.
+     * @param dimensions The dimensions of the game.
+     */
+    private void initializeOverlays(Dimensions dimensions) {
+        this.overlays.add(
+                new InventoryOverlay(dimensions, INVENTORY_SIZE));
+        this.overlays.add(new ResourceOverlay(dimensions));
+    }
+
+    /** * Initializes the inventory with default items.
+     * @param inventory The inventory to initialize.
+     */
     private void inventoryInitialiser(Inventory inventory) {
         Item[] defaultItems = {
                 new Bucket(),
@@ -190,6 +264,7 @@ public class JavaBeanFarm implements Game {
             inventory.setItem(i, defaultItems[i]);
         }
     }
+
     /**
      * Ticks the internal game state forward by one frame. a
      *
