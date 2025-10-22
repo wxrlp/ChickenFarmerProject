34afbabf04d5756ded304f3ee8ee0df565ea2b8c
Adin Sreekesh <adin.sreekesh@gmail.com>
2025-10-22 21:12:15 +1000

    Constructor was made private and now throws error

--- src/builder/world/WorldBuilder.java
+++ src/builder/world/WorldBuilder.java
@@ -14,58 +14,82 @@ import java.util.List;
 /**
  * Load an instance of a world from a string representation.
  *
- * <p>Each line of the file, separated by new line characters, corresponds to a row of tiles in the
- * world. Each character represents a tile according to {@link TileFactory#fromSymbol(int, int,
+ * <p>Each line of the file, separated by new line characters,
+ * corresponds to a row of tiles in the
+ * world. Each character represents a tile according to
+ * {@link TileFactory#fromSymbol(int, int,
  * char)}.
  *
  * @stage2
  */
-public class WorldBuilder {
+public
+class WorldBuilder {
 
     /**
      * Construct a new world builder.
      *
      * @hint You do not need to implement this.
      */
-    public WorldBuilder() {}
+    private
+    WorldBuilder() {
+        throw new AssertionError("WorldBuilder is" +
+                " a utility class, should not be instantiated");
+    }
 
     /**
-     * Read the encoded world text and construct the corresponding list of tiles.
+     * Read the encoded world text and construct the corresponding
+     * list of tiles.
      *
-     * <p>Each line in the text corresponds to a horizontal row of tiles. Each character correspond
-     * to a tile. A character in the file at line 3, character 10, will correspond to a tile at y=2
+     * <p>Each line in the text corresponds to a horizontal row of
+     * tiles. Each character correspond
+     * to a tile. A character in the file at line 3, character 10,
+     * will correspond to a tile at y=2
      * and x=9.
      *
-     * <p>The character in the encoding indicates the type of tile to construct based on {@link
+     * <p>The character in the encoding indicates the type of tile
+     * to construct based on {@link
      * TileFactory#fromSymbol(int, int, char)}.
      *
-     * <p>The number of lines and length of those lines must correspond to the dimensions provided.
-     * For example, if the window size is 800 and the tile size is 25 then we expect (800/25 =) 32
-     * tiles so there must be 32 lines of text and each line must have 32 characters. Otherwise, a
+     * <p>The number of lines and length of those lines must
+     * correspond to the dimensions provided.
+     * For example, if the window size is 800 and the tile size is
+     * 25 then we expect (800/25 =) 32
+     * tiles so there must be 32 lines of text and each line must
+     * have 32 characters. Otherwise, a
      * {@link WorldLoadException} is thrown.
      *
-     * @param dimensions The dimensions of the world. The tile encoding must correspond to these
-     *     dimensions.
-     * @param text The text encoding of a world.
+     * @param dimensions The dimensions of the world. The tile
+     *                   encoding must correspond to these
+     *                   dimensions.
+     * @param text       The text encoding of a world.
      * @return A list of tiles loaded from the given string.
-     * @throws WorldLoadException If the number of lines doesn't match the required amount according
-     *     to the dimensions.
-     * @throws WorldLoadException If the length of any line doesn't match the required amount
-     *     according to the dimensions.
-     * @throws WorldLoadException If any character doesn't correspond to a tile according to {@link
-     *     TileFactory#fromSymbol(int, int, char)}.
+     * @throws WorldLoadException If the number of lines doesn't
+     *                            match the required amount according
+     *                            to the dimensions.
+     * @throws WorldLoadException If the length of any line doesn't
+     *                            match the required amount
+     *                            according to the dimensions.
+     * @throws WorldLoadException If any character doesn't
+     *                            correspond to a tile according to
+     *                            {@link
+     *                            <p>
+     *                            <p>
+     *                            <p>
+     *                            <p>
+     *                            TileFactory#fromSymbol(int, int,
+     *                            char)}.
      */
-    public static List<Tile> fromString(Dimensions dimensions, String text)
+    public static
+    List<Tile> fromString(Dimensions dimensions, String text)
             throws WorldLoadException {
-        int numberOfTiles = dimensions.windowSize() / dimensions.tileSize();
+        int numberOfTiles =
+                dimensions.windowSize() / dimensions.tileSize();
         String[] lines = text.split("\n");
         final boolean lineDesync = lines.length != numberOfTiles;
         if (lineDesync) {
-            throw new WorldLoadException(
-                    "Expected "
-                            + numberOfTiles
-                            + " lines to match the given dimensions but got "
-                            + lines.length);
+            throw new WorldLoadException("Expected " + numberOfTiles +
+                    " lines to match the given dimensions " +
+                    "but got " + lines.length);
         }
 
         final List<Tile> tiles = new ArrayList<>();
@@ -74,11 +98,10 @@ public class WorldBuilder {
 
             if (currentRow.length != numberOfTiles) {
                 throw new WorldLoadException(
-                        "Expected "
-                                + numberOfTiles
-                                + " characters to match the given dimensions but got "
-                                + currentRow.length,
-                        row);
+                        "Expected " + numberOfTiles +
+                                " characters to match the given " +
+                                "dimensions but got " +
+                                currentRow.length, row);
             }
 
             for (int col = 0; col < numberOfTiles; col++) {
@@ -87,9 +110,13 @@ public class WorldBuilder {
                 char symbol = currentRow[col];
                 Tile tile;
                 try {
-                    tile = TileFactory.fromSymbol(tileX, tileY, symbol);
+                    tile = TileFactory.fromSymbol(
+                            tileX, tileY,
+                            symbol);
                 } catch (IllegalArgumentException e) {
-                    throw new WorldLoadException("Unknown symbol: '" + symbol + "'", row, col);
+                    throw new WorldLoadException(
+                            "Unknown symbol: '" + symbol + "'", row,
+                            col);
                 }
                 tiles.add(tile);
             }
@@ -98,21 +125,28 @@ public class WorldBuilder {
     }
 
     /**
-     * Read the provided file and attempt to create a new world based on the tile encoding in the
+     * Read the provided file and attempt to create a new world
+     * based on the tile encoding in the
      * file.
      *
-     * <p>See {@link #fromString(Dimensions, String)} for a description of how the tile encoding is
+     * <p>See {@link #fromString(Dimensions, String)} for a
+     * description of how the tile encoding is
      * read.
      *
-     * @param dimensions The dimensions of the world. The tile encoding must correspond to these
-     *     dimensions.
-     * @param filepath The path to a file containing a tile encoding.
+     * @param dimensions The dimensions of the world. The tile
+     *                   encoding must correspond to these
+     *                   dimensions.
+     * @param filepath   The path to a file containing a tile
+     *                   encoding.
      * @return A new world containing all tiles in the specified file.
-     * @throws IOException If the file path doesn't exist or otherwise can't be read.
-     * @throws WorldLoadException If the tile encoding is invalid (according to {@link
-     *     #fromString(Dimensions, String)}).
+     * @throws IOException        If the file path doesn't exist or
+     *                            otherwise can't be read.
+     * @throws WorldLoadException If the tile encoding is invalid
+     *                            (according to {@link
+     *                            #fromString(Dimensions, String)}).
      */
-    public static BeanWorld fromFile(Dimensions dimensions, String filepath)
+    public static
+    BeanWorld fromFile(Dimensions dimensions, String filepath)
             throws IOException, WorldLoadException {
         String text = Files.readString(Path.of(filepath));
         return fromTiles(fromString(dimensions, text));
@@ -123,19 +157,23 @@ public class WorldBuilder {
      *
      * @return A new empty world.
      */
-    public static BeanWorld empty() {
+    public static
+    BeanWorld empty() {
         return new BeanWorld();
     }
 
     /**
-     * Construct a new world containing all the tiles in the parameter.
+     * Construct a new world containing all the tiles in the
+     * parameter.
      *
      * @param tiles Tiles to populate the world.
      * @return A new world containing all given tiles.
      */
-    public static BeanWorld fromTiles(List<Tile> tiles) {
+    public static
+    BeanWorld fromTiles(List<Tile> tiles) {
         BeanWorld world = new BeanWorld();
-        for (Tile tile : tiles.reversed()) { // reverse so tests don't implicitly rely on order
+        for (Tile tile : tiles.reversed()) { // reverse so tests
+            // don't implicitly rely on order
             world.place(tile);
         }
         return world;
